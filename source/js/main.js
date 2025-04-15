dd370ad7cef824c143d3511b0ac50b38f53dc50f
eka-jashina <142108910+eka-jashina@users.noreply.github.com>
2025-04-15 12:49:57 +0000

    edit swiper

--- source/js/main.js
+++ source/js/main.js
@@ -25,8 +25,25 @@ const heroSwiper = new Swiper('.hero__swiper', {
   modules: [Pagination],
   slidesPerView: 1,
   loop: true,
+  speed: 900,
   pagination: {
-    el: '.hero__pagination',
+    el: '.hero__navigation',
     type: 'bullets',
+    bulletElement: 'button',
+    bulletClass: 'hero__bullet',
+    bulletActiveClass: 'hero__bullet--active',
+    clickable: true,
   },
+  on: {
+    slideChange: function () {
+      this.slides.forEach((slide, index) => {
+        const isActive = index === this.activeIndex;
+        slide.querySelectorAll('a, button, input, textarea, select, [tabindex]')
+          .forEach(el => {
+            el.tabIndex = isActive ? 0 : -1;
+          });
+      });
+    },
+  },
+
 });
